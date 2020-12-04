9c25f1169328fe00331a4e2cf7a4607e0e6ced25
CityMado <stenius.jere@gmail.com>
2020-12-04 13:41:16 +0200

    added chapter3 app

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-#kommentti
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from Config import Config
+from extensions import db
+from models.user import User
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(InventoryListResource, '/inventories')
+    api.add_resource(InventoryResource, '/inventories/<int:inventory_id>')
+    api.add_resource(InventoryPublishResource, '/inventories/<int:inventory_id>/publish')
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
